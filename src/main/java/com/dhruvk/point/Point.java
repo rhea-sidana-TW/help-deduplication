11a935f2db72bac216d13447fb84c83738bd1591
Rhea Sidana <rhea.sidana@Rheas-MacBook-Pro.local>
2022-02-23 00:55:02 +0530

    Initial commit

--- src/main/java/com/dhruvk/point/Point.java
+++ src/main/java/com/dhruvk/point/Point.java
@@ -14,8 +14,7 @@ public class Point {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Point point = (Point) o;
-        return x == point.x &&
-                y == point.y;
+        return (this.x == point.x && this.y == point.y);
     }
 
 }
